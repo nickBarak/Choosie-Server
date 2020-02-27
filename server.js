789fd23f6ade19f41691c3aaaf4d9ac87b84f7f0
nickBarak <nicholasjbarak@gmail.com>
2020-02-27 01:28:59 -0800

    cors again

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ client.connect()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('views'));
 
+app.use( (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://choosie.us');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 const index = require('./controllers/index');
 const start = require('./controllers/start/controller');
 const myList = require('./controllers/myList/controller');
