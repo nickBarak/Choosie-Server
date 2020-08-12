ec6d501ab5b6f59e21da4f91232f7847fefe2392
nickBarak <nicholasjbarak@gmail.com>
2020-08-12 01:37:10 -0700

    ..

--- Functions.js
+++ Functions.js
@@ -6,7 +6,7 @@ async function queryDB(response, query, queryparams) {
         const { rows } = await client.query(query, queryparams);
         response.json(rows);
     } catch (e) { console.log(e) }
-    finally { client.release() }
+    finally { client && client.release() }
 }
 
 exports.queryDB = queryDB
