030836d4fa37c550bc37d6cbf2c93233aa6d2dd9
nickBarak <nicholasjbarak@gmail.com>
2020-08-30 21:04:09 -0700

    .

--- controllers/custom.js
+++ controllers/custom.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { queryDB } = require('../Functions');
+const { pool } = require('../server');
 
 router.get('/', async (req, res) => {
-    for (let i = 1; i < 84894; i += 4) {
-        await queryDB(res, `DELETE FROM popular_trending WHERE id = ANY('{${i, i+1, i+2}}')`);
-        await queryDB(res, `DELETE FROM popular_recent_releases WHERE id = ANY('{${i+2, i+3, i+4}}')`);
-        await queryDB(res, `DELETE FROM popular_most_saved_this_month WHERE id = ANY('{${i+3, i+4, i+5}}')`);
-        await queryDB(res, `DELETE FROM popular_most_saved WHERE id = ANY('{${i+4,i+5,i+6}}')`);
-    }
-    res.send('Items deleted');
-})
+    try {
+        var client = await pool.connect();
+        for (let i = 1; i < 84888; i += 4) {
+            await queryDB(res, `DELETE FROM popular_trending WHERE id = ANY('{${i, i+1, i+2}}')`);
+            await queryDB(res, `DELETE FROM popular_recent_releases WHERE id = ANY('{${i+2, i+3, i+4}}')`);
+            await queryDB(res, `DELETE FROM popular_most_saved_this_month WHERE id = ANY('{${i+3, i+4, i+5}}')`);
+            await queryDB(res, `DELETE FROM popular_most_saved WHERE id = ANY('{${i+4,i+5,i+6}}')`);
+        }
+        res.send('Items deleted');
+    } catch (e) { console.log(e) }
+    finally { client && client.release() }
+});
 
 module.exports = router
