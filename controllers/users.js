6a8d28d91dfed2ba1dfd4cd41c4f0da834ac845b
nickBarak <nicholasjbarak@gmail.com>
2020-08-31 16:35:50 -0700

    .

--- controllers/users.js
+++ controllers/users.js
@@ -48,7 +48,7 @@ router.post('/', async (req, res) => {
         await client.query(
             `INSERT INTO users
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)`,
-            [name, email, username, await bcypt.hash(password, 10), age, sex, '{}', `{${languages ? languages.toString() : null}}`, 0, 0, 0, 0, 3, '{}', '{}', '{}', '{}', '{}', '{}', true, '{}']);
+            [name, email, username, await bcrypt.hash(password, 10), age, sex, '{}', `{${languages ? languages.toString() : null}}`, 0, 0, 0, 0, 3, '{}', '{}', '{}', '{}', '{}', '{}', true, '{}']);
         const result = await client.query(`SELECT * FROM users WHERE username = $1`, [username]);
         res.json(result.rows[0]);
     } catch (e) { console.log(e) }
