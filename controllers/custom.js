e4d037473bcaa50b3b3ab046770c0bbb1f008de2
nickBarak <nicholasjbarak@gmail.com>
2020-08-30 21:11:44 -0700

    .

--- controllers/custom.js
+++ controllers/custom.js
@@ -5,7 +5,7 @@ const { pool } = require('../server');
 router.get('/', async (req, res) => {
     try {
         var client = await pool.connect();
-        for (let i = 1; i < 84888; i += 4) {
+        for (let i = 1; i < 10000/*84888*/; i += 4) {
             await client.query(`DELETE FROM popular_trending WHERE id = ANY('{${i, i+1, i+2}}')`);
             await client.query(`DELETE FROM popular_recent_releases WHERE id = ANY('{${i+2, i+3, i+4}}')`);
             await client.query(`DELETE FROM popular_most_saved_this_month WHERE id = ANY('{${i+3, i+4, i+5}}')`);
