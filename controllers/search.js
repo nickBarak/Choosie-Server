301958a3a21444707f7ea23191e1d632297835dd
nickBarak <nicholasjbarak@gmail.com>
2020-09-02 04:05:17 -0700

    Major changes to search process

--- controllers/search.js
+++ controllers/search.js
@@ -3,7 +3,34 @@ const router = express.Router();
 const { pool } = require('../server');
 const { queryDB } = require('../Functions');
 
-router.get('/', async (req, res) => {
+router.get('/title', async (req, res) => {
+    try {
+        var client = await pool.connect();
+        let { rows } = await client.query(
+            `SELECT *
+            FROM (
+                SELECT DISTINCT ON
+                    (id) id,
+                    json_build_object('id', id, 'title', title, 'mpaa_rating', mpaa_rating, 'duration_in_mins', duration_in_mins, 'cover_file', cover_file, 'release_date', release_date, 'genres', genres),
+                    title
+                FROM movie_data) x
+            WHERE
+                LOWER(title) = LOWER('${req.query.search.replace(/'/g, "''")}')
+                OR LOWER(title) LIKE LOWER('% ${req.query.search.replace(/'/g, "''")} %')
+                OR LOWER(title) LIKE LOWER('${req.query.search.replace(/'/g, "''")} %')
+                OR LOWER(title) LIKE LOWER('% ${req.query.search.replace(/'/g, "''")}')`
+            // OFFSET $1 ROWS
+            // FETCH NEXT 11 ROWS ONLY`
+            // , [Number(req.query.page) * 10 - 10 || '0']);
+        );
+
+        await client.query(`UPDATE users SET search_history = array_append(search_history, $1) WHERE username = $2`, [req.query.search, req.query.user ? req.query.user : 'anonymous']);
+        res.json(rows.length ? rows.map(row => row.json_build_object) : []);
+    } catch (e) { console.log(e) }
+    finally { client && client.release() }
+});
+
+router.get('/people', async (req, res) => {
     try {
         var client = await pool.connect();
         let { rows } = await client.query(
@@ -14,31 +41,100 @@ router.get('/', async (req, res) => {
                     json_build_object('id', id, 'title', title, 'mpaa_rating', mpaa_rating, 'duration_in_mins', duration_in_mins, 'cover_file', cover_file, 'release_date', release_date, 'genres', genres),
                     unnest(actors) actor,
                     unnest(writers) writer,
-                    unnest(directors) director,
-                    unnest(genres) genre,
-                    title,
-                    description,
-                    cover_file,
-                    mpaa_rating,
-                    duration_in_mins,
-                    release_date,
-                    genres
+                    unnest(directors) director
                 FROM movie_data) x
             WHERE
-                LOWER(title) LIKE LOWER('%${req.query.search.replace(/'/g, "''")}%')
-                OR LOWER(actor) LIKE LOWER('%${req.query.search.replace(/'/g, "''")}%')
-                OR LOWER(writer) LIKE LOWER('%${req.query.search.replace(/'/g, "''")}%')
-                OR LOWER(director) LIKE LOWER('%${req.query.search.replace(/'/g, "''")}%')
-                OR LOWER(genre) LIKE LOWER('%${req.query.search.replace(/'/g, "''")}%')
-                OR LOWER(description) LIKE LOWER('%${req.query.search.replace(/'/g, "''")}%')
-            OFFSET $1 ROWS
-            FETCH NEXT 11 ROWS ONLY`, [Number(req.query.page) * 10 - 10 || '0']);
-
-        await client.query(`UPDATE users SET search_history = array_append(search_history, $1) WHERE username = $2`, [req.query.search, req.query.user ? req.query.user : 'anonymous']);
+                LOWER(actor) = LOWER('${req.query.search.replace(/'/g, "''")}')
+                OR LOWER(actor) LIKE LOWER('${req.query.search.replace(/'/g, "''")} %')
+                OR LOWER(actor) LIKE LOWER('% ${req.query.search.replace(/'/g, "''")}')
+                OR LOWER(director) = LOWER('${req.query.search.replace(/'/g, "''")}')
+                OR LOWER(director) LIKE LOWER('${req.query.search.replace(/'/g, "''")} %')
+                OR LOWER(director) LIKE LOWER('% ${req.query.search.replace(/'/g, "''")}')
+                OR LOWER(writer) = LOWER('${req.query.search.replace(/'/g, "''")}')
+                OR LOWER(writer) LIKE LOWER('${req.query.search.replace(/'/g, "''")} %')
+                OR LOWER(writer) LIKE LOWER('% ${req.query.search.replace(/'/g, "''")}')`/*,
+            [Number(req.query.page) * 10 - 10 || '0']*/);
         res.json(rows.length ? rows.map(row => row.json_build_object) : []);
     } catch (e) { console.log(e) }
     finally { client && client.release() }
 });
+
+router.get('/genre', async (req, res) => {
+    try {
+        var client = await pool.connect();
+        let { rows } = await client.query(
+            `SELECT *
+            FROM (
+                SELECT DISTINCT ON
+                    (id) id,
+                    json_build_object('id', id, 'title', title, 'mpaa_rating', mpaa_rating, 'duration_in_mins', duration_in_mins, 'cover_file', cover_file, 'release_date', release_date, 'genres', genres),
+                    unnest(genres) genre
+                FROM movie_data) x
+            WHERE
+                LOWER(genre) = LOWER('${req.query.search.replace(/'/g, "''")}')`/*,
+            [Number(req.query.page) * 10 - 10 || '0']*/);
+        res.json(rows.length ? rows.map(row => row.json_build_object) : []);
+    } catch (e) { console.log(e) }
+    finally { client && client.release() }
+});
+
+router.get('/description', async (req, res) => {
+    try {
+        var client = await pool.connect();
+        let { rows } = await client.query(
+            `SELECT *
+            FROM (
+                SELECT DISTINCT ON
+                    (id) id,
+                    json_build_object('id', id, 'title', title, 'mpaa_rating', mpaa_rating, 'duration_in_mins', duration_in_mins, 'cover_file', cover_file, 'release_date', release_date, 'genres', genres),
+                    description
+                FROM movie_data) x
+            WHERE
+                LOWER(description) LIKE LOWER('% ${req.query.search.replace(/'/g, "''")} %')`/*,
+            [Number(req.query.page) * 10 - 10 || '0']*/);
+        res.json(rows.length ? rows.map(row => row.json_build_object) : []);
+    } catch (e) { console.log(e) }
+    finally { client && client.release() }
+});
+
+router.get('/release-date', async (req, res) => {
+    try {
+        var client = await pool.connect();
+        let { rows } = await client.query(
+            `SELECT *
+            FROM (
+                SELECT DISTINCT ON
+                    (id) id,
+                    json_build_object('id', id, 'title', title, 'mpaa_rating', mpaa_rating, 'duration_in_mins', duration_in_mins, 'cover_file', cover_file, 'release_date', release_date, 'genres', genres),
+                    EXTRACT ( year from release_date ) AS "year"
+                FROM movie_data) x
+            WHERE
+                year::VARCHAR = '${req.query.search.replace(/'/g, "''")}'`/*,
+            [Number(req.query.page) * 10 - 10 || '0']*/);
+        res.json(rows.length ? rows.map(row => row.json_build_object) : []);
+    } catch (e) { console.log(e) }
+    finally { client && client.release() }
+});
+
+router.get('/age-rating', async (req, res) => {
+    try {
+        var client = await pool.connect();
+        let { rows } = await client.query(
+            `SELECT *
+            FROM (
+                SELECT DISTINCT ON
+                    (id) id,
+                    json_build_object('id', id, 'title', title, 'mpaa_rating', mpaa_rating, 'duration_in_mins', duration_in_mins, 'cover_file', cover_file, 'release_date', release_date, 'genres', genres),
+                    mpaa_rating
+                FROM movie_data) x
+            WHERE
+                LOWER(mpaa_rating) = LOWER('${req.query.search.replace(/'/g, "''")}')`/*,
+            [Number(req.query.page) * 10 - 10 || '0']*/);
+        res.json(rows.length ? rows.map(row => row.json_build_object) : []);
+    } catch (e) { console.log(e) }
+    finally { client && client.release() }
+});
+
 
 router.post('/', async (req, res) =>
     queryDB(res,
